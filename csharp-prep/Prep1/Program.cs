ea5e021f4127ce7eb32cbbfcb7800aaafb5a8285
Jade Miller <mil19017@byui.edu>
2023-04-25 14:29:35 -0600

    prepone

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Asks for user input for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Asks for user input for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Prints input in sentance "Your name is Burton, Scott Burton."
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
