dcd8c236b30b6c63a8f791335ef8c97cad6def0e
Jade Miller <mil19017@byui.edu>
2023-04-25 15:00:31 -0600

    Preptwo

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        // puting grade percentage into a string
+        string grade_str = Console.ReadLine();
+        // converting string into an integer
+        int grade = int.Parse (grade_str);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine($"A - {grade}%");
+            if (grade <= 70)
+            {
+                Console.WriteLine("With this grade percentage, you have failed the course. We encourage you to try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congradulations, with this grade you have passed this course!");
+            }
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine($"B - {grade}%");
+            if (grade <= 70)
+            {
+                Console.WriteLine("With this grade percentage, you have failed the course. We encourage you to try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congradulations, with this grade you have passed this course!");
+            }
+            }
+        else if (grade >= 70)
+        {
+            Console.WriteLine($"C - {grade}%");
+            if (grade <= 70)
+            {
+                Console.WriteLine("With this grade percentage, you have failed the course. We encourage you to try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congradulations, with this grade you have passed this course!");
+            }
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine($"D - {grade}%");
+            if (grade <= 70)
+            {
+                Console.WriteLine("With this grade percentage, you have failed the course. We encourage you to try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congradulations, with this grade you have passed this course!");
+            }
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine($"F - {grade}%");
+            if (grade <= 70)
+            {
+                Console.WriteLine("With this grade percentage, you have failed the course. We encourage you to try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congradulations, with this grade you have passed this course!");
+            }
+        }
     }
 }
