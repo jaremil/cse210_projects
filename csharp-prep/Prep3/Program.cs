e7bb83b0f4cd903eab1fcfe65b7534a9f2da406b
Jade Miller <mil19017@byui.edu>
2023-04-27 14:54:45 -0600

    inclasswork

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random generator = new Random();
+        int secret_number = generator.Next(1,3);
+
+        Console.Write("What is your guess? ");
+        string guess_str = Console.ReadLine();
+        int guess = int.Parse (guess_str);
+
+        bool countinue = true;
+
+        while (true) {
+            if (guess > secret_number) {
+                
+                Console.WriteLine($"{guess} is too high! Guess again");
+                bool countinue = true;
+
+            } else if (guess < secret_number){
+                
+                Console.WriteLine($"{guess} is too low! Guess again");
+                bool countinue = true;
+
+            } else if (guess == secret_number) {
+                
+                Console.WriteLine($"You're right! The number was {guess}");
+                bool countinue = false;
+
+            } 
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        var numbers = new List<int>();
+
+        while (true) {
+            Console.Write("Please enter a number (enter 0 to stop): ");
+
+            int number = int.Parse(Console.ReadLine());
+            
+            if (number == 0) {
+                break;
+            }
+
+            numbers.Add(number);
+        
+        }
+
+        foreach (var num in numbers) {
+            int number_added = 0;
+
+            
+
+        }
     }
 }
