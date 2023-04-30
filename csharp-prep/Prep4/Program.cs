ecdf55ce436a52a8564f85c64d5974d2bb3355eb
Jade Miller <mil19017@byui.edu>
2023-04-29 22:34:35 -0600

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,30 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var numbers = new List<int>();
-
-        while (true) {
-            Console.Write("Please enter a number (enter 0 to stop): ");
-            int number = int.Parse(Console.ReadLine());
-            
-            if (number == 0) {
-                break;
-            }
-
-            numbers.Add(number);
+        List<int> numbers = new List<int>();
         
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
         }
 
         int sum = 0;
-
-        foreach (var num in numbers) {
-            sum+= num;
+        foreach (int number in numbers)
+        {
+            sum += number;
         }
 
-            float average =(float)sum / (float)numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
 
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
