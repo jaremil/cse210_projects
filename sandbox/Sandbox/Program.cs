39716f4563d5b85454d39f807c1947f6b52dcb5e
Jade Miller <mil19017@byui.edu>
2023-05-02 14:40:02 -0600

    classnotes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        var personOne = new Person("Layne", "Moseley");
+        var personTwo = new Person("David", "Hasselhoff");
+
+        personOne.Talk();
+        personTwo.Talk();
+    }
+}
+
+
+// class means classification
+// public = can be used in other places
+// void = doesn't return a value
+class Person {
+
+    // attributes / properties
+
+    string firstName;
+    string lastName;
+
+    // constructor 
+
+    public Person(string fn, string ln) {
+        firstName = fn;
+        lastName = ln;
+    }
+
+    //behaviors
+    public void Breathe() {
+        Console.WriteLine("Breathing");
+    }
+
+    public void Walk() {
+        Console.WriteLine("Walking");
+    }
+
+    public void Talk(){
+        Console.WriteLine($"Hello, my name is {FullName()}.");
+    }
+
+    public string FullName() {
+
+        return $"{firstName} {lastName}";
+
+        // This code is another option to do the same thing
+        // public void FullName() {
+        // string full = String.Concat($"{firstName} {lastName}");
+        // return full;
+        //}
+
     }
 }
