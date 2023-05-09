f19ae68f5e7cc3f6a465014b7a0c5b1fc0915e68
Jade Miller <mil19017@byui.edu>
2023-05-09 14:56:34 -0600

    class work

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,23 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        var companyOne = new Job("");
-        
+        Job job_one = new Job();
+        job_one.jobTitle = "";
+        job_one.company = "";
+        job_one.startYear = ;
+        job_one.endYear = ;
+
+        Job job_two = new Job();
+        job_two.jobTitle = "";
+        job_two.company = "";
+        job_two.startYear = ;
+        job_two.endYear = ;
+
+        Resume officalResume = new Resume();
+        officalResume.name = "";
+        Resume.job_career.Add(job_one);
+        Resume.job_career.Add(job_two);
+
+        officalResume.Display();
     }
 }
 
-class Job
+public class Job
 {
+    string company;
+     public Job(string jt) {
+        company = jt;
+    }
 
-   string company;
+   string jobTitle;
+     public Job(string jtt) {
+        jobTitle = jtt;
+    }
 
+   string startYear;
+     public Job(int sy) {
+        startYear = sy;
+    }
+
+   int endYear;
+     public Job(int ey) {
+        endYear = ey;
+    }
 }
 
-class Resume
+public class Resume
 {
+    public string _name;
 
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
 }
-
-// public string JobInfo() {
-//         return $"{Job Title} ({Company}) {StartYear}-{EndYear}";
-//     }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,52 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
+// Program
+// userInput: int
+// menu(): int
+class Program{
+    static void Menu(int[] args) {
+    
     }
+
+    public int userInput = 
+}
+
+// PromptGenorator
+// prompts: List<strings>
+// random(): string
+class PromptGenorator{
+    
+   var prompts = new List<String> {
+    
+    };
+        
+    static void Random(string[] arg) {
+
+    }
+}
+
+// Journal
+// listEntries: List<Entry>
+// displayEntryList(Entry):
+// List<string>
+// saveToFile(): string
+// loadToFile(): void
+class Journal{
+    var listEntries = new List<Entry> {
+    
+    };
+
+    public string displayEntryList(Entry){
+
+    }
+
+}
+
+// Entry
+// random: string
+// date: string
+// userAnswer: string
+// displayEntry(): string
+class Entry{
+
 }
