ef6ae3e116a9815b7f817c8b52348ec217cbf61e
Jade Miller <mil19017@byui.edu>
2023-05-16 14:58:36 -0600

    class work

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,29 +1,17 @@
 using System;
 
-class Program{
-
-static void Main(string[] args)
+class Program
 {
-    var savings = new Account();
-    savings.Deposit(500);
-    savings.Deposit(1000);
-}
-    public class Account{
-        // private means that it can only be used inside of this class. If it is tried to be used outside 
-        // of the class then an error stating that it is not accessible because of it's level
-        private List<int> _transactions = new List<int>();
 
-        public void Deposit(int amount){
-            _transactions.Add(amount);
-        }
+    
 
-        public int Balance(){
-            var balance = 0;
-            foreach (var transaction in _transactions){
-                balance += transaction;
-            }
+    static void Fraction()
+    {
 
-            return balance;
-        }
+    }
+
+    static void GetTop()
+    {
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -54,3 +54,97 @@ class Person {
 
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Job job_one = new Job();
+        job_one.jobTitle = "";
+        job_one.company = "";
+        job_one.startYear = ;
+        job_one.endYear = ;
+
+        Job job_two = new Job();
+        job_two.jobTitle = "";
+        job_two.company = "";
+        job_two.startYear = ;
+        job_two.endYear = ;
+
+        Resume officalResume = new Resume();
+        officalResume.name = "";
+        Resume.job_career.Add(job_one);
+        Resume.job_career.Add(job_two);
+
+        officalResume.Display();
+    }
+}
+
+public class Job
+{
+    string company;
+     public Job(string jt) {
+        company = jt;
+    }
+
+   string jobTitle;
+     public Job(string jtt) {
+        jobTitle = jtt;
+    }
+
+   string startYear;
+     public Job(int sy) {
+        startYear = sy;
+    }
+
+   int endYear;
+     public Job(int ey) {
+        endYear = ey;
+    }
+}
+
+public class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
+class Program{
+
+static void Main(string[] args)
+{
+    var savings = new Account();
+    savings.Deposit(500);
+    savings.Deposit(1000);
+}
+    public class Account{
+        // private means that it can only be used inside of this class. If it is tried to be used outside 
+        // of the class then an error stating that it is not accessible because of it's level
+        private List<int> _transactions = new List<int>();
+
+        public void Deposit(int amount){
+            _transactions.Add(amount);
+        }
+
+        public int Balance(){
+            var balance = 0;
+            foreach (var transaction in _transactions){
+                balance += transaction;
+            }
+
+            return balance;
+        }
+    }
+}
