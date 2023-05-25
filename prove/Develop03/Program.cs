ca528f486eb399ac6aabea536f2afe678d06f4ef
Jade Miller <mil19017@byui.edu>
2023-05-25 15:00:21 -0600

    class work, no errors

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,6 +3,9 @@ using System;
 class Program {
     static void Main(string[] args) {
 
+        // gets user input
+        Console.WriteLine("\n Press 'enter' to change the amount of words displayed in the scripture.\n Type 'quit' before pressing 'enter' to end the program.\n");
+
     }
 }
 
@@ -27,32 +30,40 @@ class Reference {
 
 class Scripture {
     // attribute
-    string _reference;
-    string _verse;
+    Reference _reference;
+    string _text;
+    private List<Word> _words;
+
+    // declares string of scripture (still thinking through this one)
+    public string text = "!scripture here!";
 
     // constructor
-    public Scripture (string reference, string verse) {
+    public Scripture(Reference reference, string text) {
         _reference = reference;
-        _verse = verse;
+        _text = text;
+        _words = new List<Word>();
+
+        // splits string into strings per word
+        string[] words = text.Split(" ");
+        foreach (var w in words){
+            var word0bj = new Word(w);
+            _words.Add(word0bj);
+        }
     }
 
     // method
     void DisplayScripture()  {
 
     }
-
-    void Words() {
-
-    }
 }
 
 class Word {
     // attribute
-    List<string> _wordList;
+    string _letters;
 
     // constructor
-    public Word (List<string> wordList) {
-        _wordList = wordList;
+    public Word (string wordList) {
+        _letters = wordList;
     }
 
     // method
