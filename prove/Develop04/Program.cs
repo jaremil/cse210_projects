79d2cc8da6c4b5896024cee7f35a09d990806009
Jade Miller <mil19017@byui.edu>
2023-06-07 21:10:03 -0600

    bugs fixed

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,35 +3,25 @@ using System;
 class Program {
     static void Main() {
 
-        var descriptionBreathing = new Description("This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
-        var descriptionReflection = new Description("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
-        var descriptionListing = new Description("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
-        var startmessageBreathing = new Startmessage("");
-        var startmessageReflection = new Startmessage("");
-        var startmessageListing = new Startmessage("");
-        var endmessageBreathing = new Endmessage("");
-        var endmessageReflection = new Endmessage("");
-        var endmessageListing = new Endmessage("");
+        var startmessageBreathing = new Startmessage("Welcome to the Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+        var startmessageReflection = new Startmessage("Welcome to the Reflection Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+        var startmessageListing = new Startmessage("Welcome to the Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+        var endmessageBreathing = new Endmessage("You did a good job with your breathing!");
+        var endmessageReflection = new Endmessage("Good job with your reflecting!");
+        var endmessageListing = new Endmessage("That was a good list that you wrote!");
 
         ShowSpinner(5);
         Breath(8);
 
     }
 
-    class Description {
+    class Startmessage {
+        protected string _introduction;
         protected string _description;
 
-        public Description(string description){
+        public Startmessage(string introduction, string description){
+            _introduction = introduction;
             _description = description;
-        }
-    }
-
-    class Startmessage {
-        protected string _startmessage;
-
-        public Startmessage(string startmessage){
-            _startmessage = startmessage;
-
         }
     }
 
@@ -102,4 +92,5 @@ class Program {
             }
         }
     }
+
 }
